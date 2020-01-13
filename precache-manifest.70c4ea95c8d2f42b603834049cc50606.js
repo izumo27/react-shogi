57e2326cf12b70b62b56fc2369eddc66c795a7ed
izumo27 <22386882+izumo27@users.noreply.github.com>
2020-01-14 04:11:36 +0900

    Updates

---
+++ precache-manifest.70c4ea95c8d2f42b603834049cc50606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593c0a3bdbc4bfead9908bf809f72e56",
+    "url": "/react-shogi/index.html"
+  },
+  {
+    "revision": "c89191eb0413912ca68e",
+    "url": "/react-shogi/static/css/main.611395f8.chunk.css"
+  },
+  {
+    "revision": "667f7e0bae5a10112ec0",
+    "url": "/react-shogi/static/js/2.009a4bd7.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/react-shogi/static/js/2.009a4bd7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c89191eb0413912ca68e",
+    "url": "/react-shogi/static/js/main.94325fcb.chunk.js"
+  },
+  {
+    "revision": "6c0a3ea63176cb89fb5d",
+    "url": "/react-shogi/static/js/runtime-main.214ccd2c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-shogi/precache-manifest.70c4ea95c8d2f42b603834049cc50606.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-shogi/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
